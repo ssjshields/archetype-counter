b88682e0aac7ebaeacf9b2f477480ad7b610e263
AnonymousPoke <AnonPoke123@gmail.com>
2023-05-23 09:38:15 -0500

    Reloading everything into correct repo (Finally)

---
+++ Files/Counter Functions/AutoHotkey/InvokeInstall.bat
@@ -0,0 +1,13 @@
+@echo off
+
+cd /D "%~dp0"
+
+@echo on
+
+@echo:
+
+@echo  Installing AutoHotkey 1.1.36.02...
+
+@echo off
+
+start /wait AutoHotkey_1.1.36.02_setup.exe /S
---
+++ Files/Counter Functions/ImageMagick/InvokeMagickFilter.bat
@@ -0,0 +1,10 @@
+<# : InvokeMagickFilter - Solution for Archetype Counter
+@echo off
+Setlocal
+cd %~dp0
+powershell -NoLogo -Noprofile -Executionpolicy Bypass -WindowStyle Hidden -Command "Invoke-Expression $([System.IO.File]::ReadAllText('%~f0'))"
+Endlocal
+goto:eof
+#>
+
+cmd.exe /c "magick ArchetypeScreenshot.png ^ ( +clone -colorspace HSL -channel S -separate -negate -fill black -fuzz 99.9% -opaque black ) ^ -alpha off -compose copy_opacity -composite ^ -background black -alpha remove -alpha off ArchetypeScreenshotMagick.png"
